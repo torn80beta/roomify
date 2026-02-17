9f70f707b7cab530012190e2aa7f0a1fda7e15d9
Alex <103459735+torn80beta@users.noreply.github.com>
2026-02-17 13:18:38 +0200

    upload component

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,7 @@ import type { Route } from "./+types/home";
 import Navbar from "../../components/Navbar";
 import { ArrowBigRight, ArrowUpRight, Clock, Layers } from "lucide-react";
 import { Button } from "components/ui/Button";
+import Upload from "components/Upoad";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -53,7 +54,7 @@ export default function Home() {
               <p>Supports JPG, PNG, formats up to 10MB</p>
             </div>
 
-            <p>Upload images</p>
+            <Upload />
           </div>
         </div>
       </section>
